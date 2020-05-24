835067a03c91440705a01081ce0e6d70061f84a7
taku-sea <58795871+taku-sea@users.noreply.github.com>
2020-05-24 11:28:54 +0900

    Solution for ABC053
    These are solution codes for AtCoder Beginners Contest 053 at 28/01/2017.

---
+++ ABC053/ABC053_D.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll INF = 1e16;
+const ll mod = 1000000007;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+template<class T> inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template<class T> inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+int ctoi(char c) {
+  if (c >= '0' && c <= '9') {
+    return c - '0';
+  }
+  return 0;
+}
+ll factorial(ll n) {
+  if (n == 1) {
+    return 1;
+  }
+  ll ans = factorial(n-1);
+  return ans*n;
+}
+ll gcd(ll a, ll b) {
+  if (b == 0) return a;
+  return gcd(b, a%b);
+}
+ll lcm(ll a, ll b) {
+  return (a/gcd(a, b))*b;
+}
+bool is_prime(ll n) {
+  for (ll i = 2; i*i <= n; i++) {
+    if (n%i == 0) return false;
+  }
+  return n != 1;
+}
+ll mod_pow(ll x, ll n, ll mod) {
+  if (n == 0) return 1;
+  ll res = mod_pow(x*x%mod, n/2, mod);
+  if (n & 1) res = res * x % mod;
+  return res;
+}
+
+int main() {
+  ll n;
+  cin >> n;
+  map <ll, ll> score;
+  set <ll> s;
+  rep(i, n) {
+    ll a;
+    cin >> a;
+    if (score.count(a)) score.at(a)++;
+    else score[a] = 1;
+    s.insert(a);
+  }
+  ll odd = 0;
+  for (auto p : score) {
+    if (p.second%2 == 0) odd++;
+  }
+  if (odd%2) cout << s.size()-1 << endl;
+  else cout << s.size() << endl;
+}
